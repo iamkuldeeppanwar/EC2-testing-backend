8b3c4b3bf7c5a17eed5b7e341f0d42482c79988d
iamkuldeeppanwar <kuldeep10panwar@gmail.com>
2024-09-08 17:18:03 +0530

    fix port isuue

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ const express = require("express");
 
 const app = express();
 
-// Correctly setting the port to use the environment variable or default to 8080
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 6000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    name: process.env.USER || "Default User", // Use a default value if USER is not defined in .env
+    name: process.env.USER,
     message: "Hosted Successfully!",
   });
 });
 
-// Specify the port parameter in app.listen
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on ${port}`);
 });
